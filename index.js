baef89be06c53244fcdb54032cc34cda05bf94dc
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-11-04 00:34:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("text").innerHTML = "JavaScript is so cool. It lets me add text to my page programmatically.";
 });
+
+document.getElementById("text").innerHTML = "JavaScript is so cool. It lets me add text to my page programmatically.";
